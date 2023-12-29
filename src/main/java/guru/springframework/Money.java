3454850ae4cedcc1fb4fc57fde9806076dfcde6b
Rivera Ascencio <lriveraascencio@deloitte.com>
2023-12-29 17:39:09 -0600

    as money is the base, the tests could be general

--- src/main/java/guru/springframework/Money.java
+++ src/main/java/guru/springframework/Money.java
@@ -4,24 +4,27 @@ public class Money {
 
 	protected int amount;
 	protected String currency;
-	
+
 	public Money(int amount, String currency) {
 		this.amount = amount;
 		this.currency = currency;
 	}
-
+	
+	protected String currency() {
+		return currency;
+	}
 
 	public static Money dollar(int amount) {
-		return new Dollar(amount, "USD");
+		return new Money(amount, "USD");
 	}
 
 	public static Money franc(int amount) {
-		return new Franc(amount, "CHF");
+		return new Money(amount, "CHF");
 	}
 
 	public boolean equals(Object object) {
 		Money money = (Money) object;
-		return amount == money.amount && this.currency==money.currency;
+		return amount == money.amount && this.currency == money.currency;
 	}
 
 	@Override
@@ -30,9 +33,7 @@ public class Money {
 	}
 
 	public Money times(int multiplier) {
-		return new Money(amount*multiplier, this.currency);
+		return new Money(amount * multiplier, this.currency);
 	}
-	
-	
 
 }
