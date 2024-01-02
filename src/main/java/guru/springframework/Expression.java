e29ba301ffc0f84f8887120739378c2de153d39c
Rivera Ascencio <lriveraascencio@deloitte.com>
2024-01-02 13:31:07 -0600

    changing logic and adding rate

--- src/main/java/guru/springframework/Expression.java
+++ src/main/java/guru/springframework/Expression.java
@@ -2,6 +2,6 @@ package guru.springframework;
 
 public interface Expression {
 
-	Money reduce(String to);
+	Money reduce(Bank bank, String to);
 
 }
---
+++ src/main/java/guru/springframework/Pair.java
@@ -0,0 +1,36 @@
+package guru.springframework;
+
+import java.util.Objects;
+
+public class Pair {
+	private final String from;
+	private final String to;
+	
+	public Pair(String from, String to) {
+		super();
+		this.from = from;
+		this.to = to;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(from, to);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pair other = (Pair) obj;
+		return Objects.equals(from, other.from) && Objects.equals(to, other.to);
+	}
+	
+	
+	
+	
+
+}
